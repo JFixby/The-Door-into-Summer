25a8c9536fedd39cc021fb1fb4db77d2607d1b5b
J Fixby <github@jfixby.com>
2016-03-31 12:45:11 +0200

    ,,,

--- door-into-summer-game/src/com/jfixby/summer/game/SummerTheGame.java
+++ door-into-summer-game/src/com/jfixby/summer/game/SummerTheGame.java
@@ -2,6 +2,8 @@ package com.jfixby.summer.game;
 
 import com.jfixby.cmns.api.assets.AssetID;
 import com.jfixby.cmns.api.assets.Names;
+import com.jfixby.cmns.api.sys.settings.SystemSettings;
+import com.jfixby.r3.api.RedTriplaneParams;
 import com.jfixby.r3.api.game.GameLogicComponent;
 import com.jfixby.r3.api.game.LoadTask;
 import com.jfixby.r3.api.ui.GameUI;
@@ -9,7 +11,7 @@ import com.jfixby.r3.api.ui.UILoaderListener;
 
 public class SummerTheGame implements GameLogicComponent {
 
-    private long fade_time = 1500;
+    // private long fade_time = 1500;
 
     public static final AssetID game_ui_unit_id = Names.newAssetID("com.jfixby.tinto.ui.game.GameMainUI");
     public static final AssetID loader_ui_unit_id = Names.newAssetID("com.jfixby.tinto.ui.game.GameMainUI");
@@ -17,16 +19,18 @@ public class SummerTheGame implements GameLogicComponent {
     @Override
     public void startGame() {
 
-	GameUI.showLoadingScreen(loader_ui_unit_id);
+	final long logoFadeTime = SystemSettings.getLongParameter(RedTriplaneParams.DEFAULT_LOGO_FADE_TIME);
+
+	GameUI.showLoadingScreen(loader_ui_unit_id, false);
 	LoadTask task = GameUI.prepareLoadGameUITask(game_ui_unit_id);
 	UILoaderListener ui_loader_listener = new UILoaderListener() {
 
 	    @Override
 	    public void onUILoaderDone() {
-		GameUI.pushFadeOut(fade_time);
+		GameUI.pushFadeOut(logoFadeTime);
 
 		GameUI.switchToGameUI(game_ui_unit_id);
-		GameUI.pushFadeIn(fade_time);
+		GameUI.pushFadeIn(logoFadeTime);
 		GameUI.allowUserInput();
 	    }
 
