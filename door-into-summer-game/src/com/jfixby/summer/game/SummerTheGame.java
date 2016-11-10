23d55baa30f5e7d26b24338cc123686721a15d17
J Fixby <github@jfixby.com>
2016-11-10 16:14:10 +0100

    ...

--- door-into-summer-game/src/com/jfixby/summer/game/SummerTheGame.java
+++ door-into-summer-game/src/com/jfixby/summer/game/SummerTheGame.java
@@ -1,3 +1,4 @@
+
 package com.jfixby.summer.game;
 
 import com.jfixby.cmns.api.assets.AssetID;
@@ -11,32 +12,32 @@ import com.jfixby.r3.api.ui.UILoaderListener;
 
 public class SummerTheGame implements BusinessLogicComponent {
 
-    // private long fade_time = 1500;
+	// private long fade_time = 1500;
 
-    public static final AssetID game_ui_unit_id = Names.newAssetID("com.jfixby.tinto.ui.game.GameMainUI");
-    public static final AssetID loader_ui_unit_id = Names.newAssetID("com.jfixby.tinto.ui.game.GameMainUI");
+	public static final AssetID game_ui_unit_id = Names.newAssetID("com.jfixby.tinto.ui.game.GameMainUI");
+	public static final AssetID loader_ui_unit_id = Names.newAssetID("com.jfixby.tinto.ui.game.GameMainUI");
 
-    @Override
-    public void start() {
+	@Override
+	public void start () {
 
-	final long logoFadeTime = SystemSettings.getLongParameter(RedTriplaneParams.DEFAULT_LOGO_FADE_TIME);
+		final long logoFadeTime = SystemSettings.getLongParameter(RedTriplaneParams.DEFAULT_LOGO_FADE_TIME);
 
-	UI.showLoadingScreen(loader_ui_unit_id, false);
-	LoadTask task = UI.prepareLoadUITask(game_ui_unit_id);
-	UILoaderListener ui_loader_listener = new UILoaderListener() {
+		UI.showLoadingScreen(loader_ui_unit_id, false);
+		final LoadTask task = UI.prepareLoadUITask(null, game_ui_unit_id);
+		final UILoaderListener ui_loader_listener = new UILoaderListener() {
 
-	    @Override
-	    public void onUILoaderDone() {
-		UI.pushFadeOut(logoFadeTime);
+			@Override
+			public void onUILoaderDone () {
+				UI.pushFadeOut(logoFadeTime);
 
-		UI.loadUnit(game_ui_unit_id);
-		UI.pushFadeIn(logoFadeTime);
-		UI.allowUserInput();
-	    }
+				UI.loadUnit(game_ui_unit_id);
+				UI.pushFadeIn(logoFadeTime);
+				UI.allowUserInput();
+			}
 
-	};
-	UI.pushTaskToLoader(task, ui_loader_listener);
+		};
+		UI.pushTaskToLoader(task, ui_loader_listener);
 
-    }
+	}
 
 }
