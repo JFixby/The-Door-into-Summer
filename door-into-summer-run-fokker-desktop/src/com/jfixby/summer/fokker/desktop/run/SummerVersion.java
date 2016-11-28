7afe63018dae736b5355baaad8278299dedf7eaf
J Fixby <github@jfixby.com>
2016-11-28 15:18:51 +0100

    ...

---
+++ door-into-summer-run-fokker-desktop/src/com/jfixby/summer/fokker/desktop/run/SummerVersion.java
@@ -0,0 +1,10 @@
+
+package com.jfixby.summer.fokker.desktop.run;
+
+public class SummerVersion {
+
+	public static String versionName;
+	public static int versionCode;
+	public static String packageName;
+
+}
