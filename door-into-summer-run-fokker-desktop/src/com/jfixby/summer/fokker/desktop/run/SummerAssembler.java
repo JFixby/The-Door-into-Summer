d42118009a7f3d9c50d3da7139aae9f84ef5ae80
J Fixby <github@jfixby.com>
2017-02-16 14:57:30 +0100

    ...

--- door-into-summer-run-fokker-desktop/src/com/jfixby/summer/fokker/desktop/run/SummerAssembler.java
+++ door-into-summer-run-fokker-desktop/src/com/jfixby/summer/fokker/desktop/run/SummerAssembler.java
@@ -21,10 +21,8 @@ import com.jfixby.r3.ext.api.scene2d.Scene2D;
 import com.jfixby.r3.ext.api.text.R3Text;
 import com.jfixby.r3.ext.text.red.RedTriplaneText;
 import com.jfixby.r3.fokker.api.FokkerEngineAssembler;
-import com.jfixby.r3.fokker.api.UnitsSpawner;
 import com.jfixby.r3.fokker.api.assets.FokkerTextureLoader;
 import com.jfixby.r3.fokker.assets.RedFokkerTextureLoader;
-import com.jfixby.r3.fokker.backend.RedUnitSpawner;
 import com.jfixby.r3.fokker.fs.AssetsInfo;
 import com.jfixby.r3.ui.RedUIManager;
 import com.jfixby.rana.api.asset.AssetsManager;
@@ -34,61 +32,20 @@ import com.jfixby.red.engine.core.resources.RedAssetsManager;
 import com.jfixby.red.engine.scene2d.RedScene2D;
 import com.jfixby.red.triplane.resources.fsbased.RedResourcesManager;
 import com.jfixby.red.triplane.resources.fsbased.RedResourcesManagerSpecs;
-import com.jfixby.scarabei.adopted.gdx.GdxSimpleTriangulator;
-import com.jfixby.scarabei.adopted.gdx.base64.GdxBase64;
-import com.jfixby.scarabei.api.angles.Angles;
-import com.jfixby.scarabei.api.assets.Names;
-import com.jfixby.scarabei.api.base64.Base64;
-import com.jfixby.scarabei.api.collections.Collections;
 import com.jfixby.scarabei.api.collisions.Collisions;
-import com.jfixby.scarabei.api.color.Colors;
-import com.jfixby.scarabei.api.debug.Debug;
-import com.jfixby.scarabei.api.err.Err;
 import com.jfixby.scarabei.api.file.File;
 import com.jfixby.scarabei.api.file.FileSystemSandBox;
 import com.jfixby.scarabei.api.file.LocalFileSystem;
 import com.jfixby.scarabei.api.file.cache.FileCache;
-import com.jfixby.scarabei.api.geometry.Geometry;
-import com.jfixby.scarabei.api.graphs.Graphs;
-import com.jfixby.scarabei.api.input.UserInput;
-import com.jfixby.scarabei.api.io.IO;
 import com.jfixby.scarabei.api.java.gc.GCFisher;
 import com.jfixby.scarabei.api.json.Json;
-import com.jfixby.scarabei.api.log.L;
-import com.jfixby.scarabei.api.math.FloatMath;
-import com.jfixby.scarabei.api.math.IntegerMath;
-import com.jfixby.scarabei.api.math.MathTools;
-import com.jfixby.scarabei.api.math.SimpleTriangulator;
-import com.jfixby.scarabei.api.md5.MD5;
 import com.jfixby.scarabei.api.sys.Sys;
 import com.jfixby.scarabei.api.sys.settings.ExecutionMode;
 import com.jfixby.scarabei.api.sys.settings.SystemSettings;
-import com.jfixby.scarabei.api.taskman.TaskManager;
-import com.jfixby.scarabei.api.util.JUtils;
-import com.jfixby.scarabei.red.color.RedColors;
-import com.jfixby.scarabei.red.debug.RedDebug;
-import com.jfixby.scarabei.red.desktop.collections.DesktopCollections;
-import com.jfixby.scarabei.red.desktop.filesystem.win.WinFileSystem;
-import com.jfixby.scarabei.red.desktop.math.DesktopFloatMath;
-import com.jfixby.scarabei.red.desktop.sys.DesktopSystem;
-import com.jfixby.scarabei.red.err.RedError;
 import com.jfixby.scarabei.red.filesystem.cache.RedFileCache;
 import com.jfixby.scarabei.red.filesystem.sandbox.RedFileSystemSandBox;
 import com.jfixby.scarabei.red.filesystem.virtual.InMemoryFileSystem;
-import com.jfixby.scarabei.red.geometry.RedGeometry;
-import com.jfixby.scarabei.red.graphs.RedGraphs;
-import com.jfixby.scarabei.red.input.RedInput;
-import com.jfixby.scarabei.red.io.RedIO;
 import com.jfixby.scarabei.red.java.gc.RedGCFisher;
-import com.jfixby.scarabei.red.log.SimpleLogger;
-import com.jfixby.scarabei.red.math.RedAngles;
-import com.jfixby.scarabei.red.math.RedIntegerMath;
-import com.jfixby.scarabei.red.math.RedMathTools;
-import com.jfixby.scarabei.red.name.RedAssetsNamespace;
-import com.jfixby.scarabei.red.sys.RedSystemSettings;
-import com.jfixby.scarabei.red.sys.RedTaskManager;
-import com.jfixby.scarabei.red.util.RedJUtils;
-import com.jfixby.scarabei.red.util.md5.RSADataSecurityIncMD5;
 import com.jfixby.summer.assets.DoorPath;
 import com.jfixby.summer.game.SummerTheGame;
 
@@ -96,38 +53,6 @@ public class SummerAssembler implements FokkerEngineAssembler {
 
 	@Override
 	public void assembleEngine () {
-		L.installComponent(new SimpleLogger());
-
-		Err.installComponent(new RedError());
-		Debug.installComponent(new RedDebug());
-		Collections.installComponent(new DesktopCollections());
-
-		JUtils.installComponent(new RedJUtils());
-		FloatMath.installComponent(new DesktopFloatMath());
-		TaskManager.installComponent(new RedTaskManager());
-		Sys.installComponent(new DesktopSystem());
-		SystemSettings.installComponent(new RedSystemSettings());
-
-		IntegerMath.installComponent(new RedIntegerMath());
-		Names.installComponent(new RedAssetsNamespace());
-		IO.installComponent(new RedIO());
-		Graphs.installComponent(new RedGraphs());
-		SimpleTriangulator.installComponent(new GdxSimpleTriangulator());
-		Angles.installComponent(new RedAngles());
-
-		UserInput.installComponent(new RedInput());
-
-		final RedGeometry geometry = new RedGeometry();
-		Geometry.installComponent(geometry);
-		Colors.installComponent(new RedColors());
-		MathTools.installComponent(new RedMathTools());
-		// --
-		UnitsSpawner.installComponent(new RedUnitSpawner());
-		Json.installComponent("com.jfixby.cmns.adopted.gdx.json.RedJson");
-		Base64.installComponent(new GdxBase64());
-		MD5.installComponent(new RSADataSecurityIncMD5());
-
-		LocalFileSystem.installComponent(new WinFileSystem());
 
 		try {
 			this.installResources();
